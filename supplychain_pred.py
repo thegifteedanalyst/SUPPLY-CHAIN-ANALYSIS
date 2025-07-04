413266a907fa5a40c35222bcba613818c28b0fe6
The Gifted Analyst <dominicblessing792@gmail.com>
2025-07-04 22:00:04 +0100

    Update supplychain_pred.py

--- supplychain_pred.py
+++ supplychain_pred.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
 import pickle
 
 with open("supplychain_model.pkl", "rb") as f:
